ad90cb1cfff0f222ed6b2ceb8f9760f35b940e9a
Knof <fabian.knof@th-owl.de>
2023-10-27 16:05:55 +0200

    fix: undefined Problem solved

--- src/components/HomeBase.tsx
+++ src/components/HomeBase.tsx
@@ -152,6 +152,8 @@ export function HomeBase(props: any) {
       handleRespawn();
     }
   };
+  if (!characterTextures || !environmentTextures || !backgroundTextures)
+    return console.log("...Lade");
 
   if (!game) {
     return (
