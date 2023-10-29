9de59681c8d3e7764d02394fefe8cb383bffb88a
Conrad Dreyer <git@conraddreyer.com>
2023-10-29 14:38:54 +0100

    collision sprites import

---
+++ src/assets/AssetLoader/collisions.ts
@@ -0,0 +1,11 @@
+import cloud from "../collision/CollisionAction_1.png";
+import marks from "../collision/CollisionAction_2.png";
+import zap from "../collision/CollisionAction_3.png";
+import circlingStars from "../collision/CollisionAction_4.png";
+
+export const collisionAssets = {
+  cloud: cloud,
+  marks: marks,
+  zap: zap,
+  circlingStars: circlingStars,
+};
