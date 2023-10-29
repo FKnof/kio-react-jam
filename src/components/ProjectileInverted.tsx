b2250fecc97ca165c429fb9d747ae61dc8a27ad8
Knof <fabian.knof@th-owl.de>
2023-10-29 15:50:43 +0100

    ActionLine Animation Added

--- src/components/ProjectileInverted.tsx
+++ src/components/ProjectileInverted.tsx
@@ -1,8 +1,9 @@
-import { Container, Graphics, Text } from "@pixi/react";
-import { useCallback } from "react";
+import { AnimatedSprite, Container, Graphics, Text } from "@pixi/react";
+import { useCallback, useEffect, useState } from "react";
 import { PlayerProjectile } from "../interfaces/PlayerProjectiles";
 import { Sprite } from "@pixi/react";
 import CharacterSprite from "./sprites/Character";
+import { Assets } from "pixi.js";
 
 export function ProjectileInverted({
   props,
@@ -20,7 +21,7 @@ export function ProjectileInverted({
   const { x, y, vx, vy, type, ownerId, color, radius } = props;
   const width = gameWidth;
   const height = gameHeight;
-
+  const [renderedActionLine, setRenderedActionLine] = useState<any>();
   const baseRotation = Math.PI / 2;
   const movementRotation = Math.atan2(vy, vx);
 
@@ -30,6 +31,34 @@ export function ProjectileInverted({
       : baseRotation + movementRotation;
   const scale = ownerId == yourPlayerId ? 1 : -1;
 
+  useEffect(() => {
+    async function loadTextures() {
+      const actionLinePaper = await Assets.load(
+        "./src/assets/Animations/ActionLine_Paper.json"
+      );
+
+      const actionLineStone = await Assets.load(
+        "./src/assets/Animations/ActionLines_Stone.json"
+      );
+
+      const actionLineScissor = await Assets.load(
+        "./src/assets/Animations/ActionLine_Scissor.json"
+      );
+      if (props.type === "paper") {
+        setRenderedActionLine(actionLinePaper.animations.ActionLines3);
+      } else if (props.type === "rock") {
+        setRenderedActionLine(actionLineStone.animations.ActionLines2);
+      } else {
+        setRenderedActionLine(actionLineScissor.animations.ActionLines1);
+      }
+    }
+    loadTextures();
+  }, []);
+
+  if (!renderedActionLine) {
+    return null;
+  }
+
   return (
     <Container
       x={width - x}
@@ -37,6 +66,15 @@ export function ProjectileInverted({
       rotation={adjustedRotation}
       scale={{ x: 1, y: scale }}
     >
+      <AnimatedSprite
+        anchor={[0.5, 1]}
+        rotation={3.1}
+        scale={0.3}
+        textures={renderedActionLine}
+        isPlaying={true}
+        initialFrame={0}
+        animationSpeed={0.1}
+      />
       <CharacterSprite
         characterTextures={characterTextures}
         type={type}
