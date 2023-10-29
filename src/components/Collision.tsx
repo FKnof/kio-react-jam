b867d87b30b0a3e8aa99ae72dede12421563f9bf
Conrad Dreyer <git@conraddreyer.com>
2023-10-29 16:51:40 +0100

    feat: dynamc collision textures. NOT WORKING

--- src/components/Collision.tsx
+++ src/components/Collision.tsx
@@ -8,17 +8,19 @@ export function Collision(props: any) {
     gameHeight,
     gameWidth,
     thisPlayer,
+    index,
   } = props;
   const x = thisPlayer == 0 ? collision.x : gameWidth - collision.x;
   const pureY = collision.y - game.baseOffset;
   const y = thisPlayer == 0 ? pureY : gameHeight - collision.y;
-  const texture = Object.values(collisionTextures)[1];
+  const textureArray = Object.keys(collisionTextures);
+  const texture = textureArray[index % textureArray.length];
   console.log(texture);
   return (
     <Sprite
       width={150}
       height={150}
-      texture={collisionTextures.zap}
+      texture={collisionTextures.texture}
       // scale={{ x: 1, y: 1 }}
       rotation={0}
       anchor={0.5}
