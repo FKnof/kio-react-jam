04f764e005c835acf627b2f67c13806bf527a3af
Conrad Dreyer <git@conraddreyer.com>
2023-10-29 23:00:32 +0100

    feat: loading screen

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,8 +1,11 @@
-import { Graphics } from "@pixi/react";
+import { Container, Graphics, Sprite, Text } from "@pixi/react";
 import { useCallback } from "react";
+import { fontstyle } from "../ui/fontstyle";
+import background from "../assets/Background/Mond.png";
 
 export default function LoadingScreen(props: any) {
-  const { gameWidth, gameHeight } = props;
+  const { gameWidth, gameHeight, players, yourPlayerId, opponentPlayerId } =
+    props;
   const loadingScreen = useCallback(
     (g: any) => {
       g.clear();
@@ -12,5 +15,64 @@ export default function LoadingScreen(props: any) {
     },
     [gameWidth, gameHeight]
   );
-  return <Graphics draw={loadingScreen} anchor={[0.5, 0.5]} />;
+  return (
+    <>
+      <Graphics draw={loadingScreen} anchor={[0.5, 0.5]} />
+      <Container x={0} y={0}>
+        <Sprite
+          width={gameWidth * 0.9}
+          height={gameHeight * 0.3}
+          image={"./Loading.png"}
+          scale={{ x: 1, y: 1 }}
+          rotation={0}
+          anchor={0}
+          x={gameWidth * 0.05}
+          y={gameHeight * 0.5}
+        />
+
+        <Sprite
+          width={gameWidth * 0.35}
+          height={gameHeight * 0.18}
+          image={players[yourPlayerId].avatarUrl}
+          // scale={{ x: 1, y: 1 }}
+          rotation={0}
+          anchor={0.5}
+          x={gameWidth * 0.3}
+          y={gameHeight * 0.3}
+        />
+        <Sprite
+          width={gameWidth * 0.35}
+          height={gameHeight * 0.18}
+          image={players[opponentPlayerId].avatarUrl}
+          // scale={{ x: 1, y: 1 }}
+          rotation={0}
+          anchor={0.5}
+          x={gameWidth * 0.7}
+          y={gameHeight * 0.3}
+        />
+
+        <Text
+          text={players[yourPlayerId].displayName}
+          anchor={[0.5, 0]}
+          x={gameWidth * 0.3}
+          y={gameHeight * 0.41}
+          style={fontstyle}
+        />
+        <Text
+          text={players[opponentPlayerId].displayName}
+          anchor={[0.5, 0]}
+          x={gameWidth * 0.7}
+          y={gameHeight * 0.41}
+          style={fontstyle}
+        />
+        <Text
+          text={"VS"}
+          anchor={[0.5, 0]}
+          x={gameWidth * 0.5}
+          y={gameHeight * 0.12}
+          style={fontstyle}
+        />
+      </Container>
+    </>
+  );
 }
