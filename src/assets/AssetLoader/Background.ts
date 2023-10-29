ebcc4530675e7aaed43a2fcf1e089d6acb0ada57
Conrad Dreyer <git@conraddreyer.com>
2023-10-29 21:33:58 +0100

    feat: changed background to single import

--- src/assets/AssetLoader/Background.ts
+++ src/assets/AssetLoader/Background.ts
@@ -1,21 +1,5 @@
-import moonURL from "../Background/Mond.png";
-import starsURL from "../Background/Stern.png";
-import starsVariantion1URL from "../Background/Sterne Variation_1.png";
-import starsVariantion2URL from "../Background/Sterne Variation_2.png";
-import starsVariantion3URL from "../Background/Sterne Variation_3.png";
-import starsVariantion4URL from "../Background/Sterne Variation_4.png";
-import starsVariantion5URL from "../Background/Sterne Variation_5.png";
-import starsVariantion6URL from "../Background/Sterne Variation_6.png";
-import starsVariantion7URL from "../Background/Sterne Variation_7.png";
+import fullBG from "../Background/background-full.jpg";
 
 export const backgroundAssets = {
-  moon: moonURL,
-  stars: starsURL,
-  starVariation1: starsVariantion1URL,
-  starVariation2: starsVariantion2URL,
-  starVariation3: starsVariantion3URL,
-  starVariation4: starsVariantion4URL,
-  starVariation5: starsVariantion5URL,
-  starVariation6: starsVariantion6URL,
-  starVariation7: starsVariantion7URL,
+  fullBG: fullBG,
 };
--- src/components/HomeBase-Background.tsx
+++ src/components/HomeBase-Background.tsx
@@ -32,67 +32,11 @@ export function HomeBaseBackground(props: any) {
       />
       <Container position={[0, 0]}>
         <Sprite
-          anchor={0.5}
+          anchor={0}
           scale={1}
-          x={60}
-          y={120}
-          texture={backgroundTextures.moon}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={230}
-          y={200}
-          texture={backgroundTextures.stars}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={270}
-          y={110}
-          texture={backgroundTextures.starVariation1}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={330}
-          y={310}
-          texture={backgroundTextures.starVariation2}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={240}
-          y={420}
-          texture={backgroundTextures.starVariation3}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={70}
-          y={320}
-          texture={backgroundTextures.starVariation4}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={150}
-          y={60}
-          texture={backgroundTextures.starVariation5}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={130}
-          y={210}
-          texture={backgroundTextures.starVariation6}
-        />
-        <Sprite
-          anchor={0.5}
-          scale={1}
-          x={50}
-          y={450}
-          texture={backgroundTextures.starVariation7}
+          x={0}
+          y={0}
+          texture={backgroundTextures.fullBG}
         />
       </Container>
     </>
