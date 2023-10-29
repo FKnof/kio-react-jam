e9a97d757d4db65cbd048d10c8863a59841040cd
Conrad Dreyer <git@conraddreyer.com>
2023-10-29 17:03:27 +0100

    feat: not working random collision

--- src/components/Collision.tsx
+++ src/components/Collision.tsx
@@ -13,14 +13,14 @@ export function Collision(props: any) {
   const x = thisPlayer == 0 ? collision.x : gameWidth - collision.x;
   const pureY = collision.y - game.baseOffset;
   const y = thisPlayer == 0 ? pureY : gameHeight - collision.y;
-  const textureArray = Object.keys(collisionTextures);
-  const texture = textureArray[index % textureArray.length];
+  const textureArray = Object.values(collisionTextures);
+  const texture = textureArray[Math.floor(Math.random() * textureArray.length)];
   console.log(texture);
   return (
     <Sprite
       width={150}
       height={150}
-      texture={collisionTextures.texture}
+      texture={texture}
       // scale={{ x: 1, y: 1 }}
       rotation={0}
       anchor={0.5}
