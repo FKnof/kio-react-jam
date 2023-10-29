c71f29d1cda00541391722c003d4d4222c5bce69
Knof <fabian.knof@th-owl.de>
2023-10-29 13:14:12 +0100

    changed absolute imports to relative imports

--- src/assets/AssetLoader/Healthbar.ts
+++ src/assets/AssetLoader/Healthbar.ts
@@ -1,15 +1,15 @@
-import greenLeftURL from "./src/assets/HealthBar/lifebar_gree_left.png";
-import greenCenterURL from "./src/assets/HealthBar/lifebar_green_center.png";
-import greenRightURL from "./src/assets/HealthBar/lifebar_green_right.png";
-import redLeftURL from "./src/assets/HealthBar/lifebar_red_left.png";
-import redCenterURL from "./src/assets/HealthBar/lifebar_red_center.png";
-import redRightURL from "./src/assets/HealthBar/lifebar_red_right.png";
+import greenLeftURL from "../HealthBar/lifebar_gree_left.png";
+import greenCenterURL from "../HealthBar/lifebar_green_center.png";
+import greenRightURL from "../HealthBar/lifebar_green_right.png";
+import redLeftURL from "../HealthBar/lifebar_red_left.png";
+import redCenterURL from "../HealthBar/lifebar_red_center.png";
+import redRightURL from "../HealthBar/lifebar_red_right.png";
 
 export const healthbarAssets = {
-  greenLeft: "./src/assets/HealthBar/lifebar_gree_left.png",
-  greenCenter: "./src/assets/HealthBar/lifebar_green_center.png",
-  greenRight: "./src/assets/HealthBar/lifebar_green_right.png",
-  redLeft: "./src/assets/HealthBar/lifebar_red_left.png",
-  redCenter: "./src/assets/HealthBar/lifebar_red_center.png",
-  redRight: "./src/assets/HealthBar/lifebar_red_right.png",
+  greenLeft: greenLeftURL,
+  greenCenter: greenCenterURL,
+  greenRight: greenRightURL,
+  redLeft: redLeftURL,
+  redCenter: redCenterURL,
+  redRight: redRightURL,
 };
